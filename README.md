15fe5bbfd9c1291e133701f2a269693201098ada
Peter Harpending <pharpend2@gmail.com>
2014-07-20 08:00:27 -0600

    Initial commit, compiles.

---
+++ README.md
@@ -0,0 +1,3 @@
+# flogger-client
+
+This is a command-line client for for the Flogger CMS.
---
+++ skel/Skel.hs
@@ -0,0 +1,11 @@
+-- |
+-- Module       : Text.Eros.Skel
+-- Description  : Description
+-- Copyright    : 2014, Peter Harpending.
+-- License      : BSD3
+-- Maintainer   : Peter Harpending <pharpend2@gmail.com>
+-- Stability    : experimental
+-- Portability  : archlinux
+--
+
+module Text.Eros.Skel where
