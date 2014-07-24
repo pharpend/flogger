9ddcbafe3d4cf025c1673decc908e215e982e24d
Peter Harpending <pharpend2@gmail.com>
2014-07-23 21:31:47 -0600

    Move parseInput to where function.

--- src/Main.hs
+++ src/Main.hs
@@ -13,13 +13,14 @@ Portability  : archlinux
 
 module Main where
 
-import qualified Data.ByteString as Bs
 import qualified Data.ByteString.Lazy as Bl
 import           Data.Monoid
 import qualified Data.Text as Ts
 import           Options.Applicative
 import           System.Exit
 import qualified System.IO as Io
+
+type Blog = Ts.Text
 
 data LfInput = LfInput { compile    :: Bool
                        , license    :: Bool
@@ -29,47 +30,45 @@ data LfInput = LfInput { compile    :: Bool
                        }
   deriving (Eq, Read, Show)
 
-parseInput :: Parser LfInput
-parseInput = LfInput
-  <$> switch (  short 'c'
-             <> long "compile"
-             <> help "Compile the input."
-             )
-  <*> switch (  short 'l'
-             <> long "license"
-             <> help "Print the license."
-             )
-  <*> option ( short 'i'
-             <> long "input-file"
-             <> help "Input file for compile."
-             <> metavar "FILE"
-             <> action "file"
-             <> value "STDIN"
-             <> showDefault
-             )
-  <*> option ( short 'f'
-             <> long "input-format"
-             <> help "Format of the output. Either 'json' or 'yaml'"
-             <> metavar "FORMAT"
-             <> completeWith ["json", "yaml"]
-             <> value "yaml"
-             <> showDefault
-             )
-  <*> option ( short 'o'
-             <> long "output-file"
-             <> help "File from which to take output."
-             <> metavar "FILE"
-             <> action "file"
-             <> value "STDOUT"
-             <> showDefault
-             )
-
 main :: IO ()
 main = do
     input <- execParser opts
-    putStrLn $ show input
+    print $ show  input
   where
     opts = info (helper <*> parseInput)
       ( fullDesc
      <> header "localflog - local client for flogger."
       )
+    parseInput :: Parser LfInput
+    parseInput = LfInput
+      <$> switch (  short 'c'
+                 <> long "compile"
+                 <> help "Compile the input."
+                 )
+      <*> switch (  short 'l'
+                 <> long "license"
+                 <> help "Print the license."
+                 )
+      <*> option ( short 'i'
+                 <> long "input-file"
+                 <> metavar "FILE"
+                 <> action "file"
+                 <> value "STDIN"
+                 <> showDefault
+                 )
+      <*> option ( short 'f'
+                 <> long "input-format"
+                 <> help "Format of the output. Either 'json' or 'yaml'"
+                 <> metavar "FORMAT"
+                 <> completeWith ["json", "yaml"]
+                 <> value "yaml"
+                 <> showDefault
+                 )
+      <*> option ( short 'o'
+                 <> long "output-file"
+                 <> metavar "FILE"
+                 <> action "file"
+                 <> value "STDOUT"
+                 <> showDefault
+                 )
+
